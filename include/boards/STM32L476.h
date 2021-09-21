303f8b2d0cae44620bc2f2a2de835e8278f5aaae
Neil Enns <info@danecreekphotography.com>
2021-09-21 06:58:18 -0700

    Fake being a mega for now

--- include/boards/STM32L476.h
+++ include/boards/STM32L476.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define MOBIFLIGHT_TYPE "MobiFlight STM32"
+#define MOBIFLIGHT_TYPE "MobiFlight Mega"
 #define MOBIFLIGHT_SERIAL "0987654321"
-#define MEMLEN_NAME "MobiFlight STM32"
+#define MEMLEN_NAME "MobiFlight Mega"
 #define EEPROM_SIZE 1024
 #define MEMLEN_CONFIG 256
