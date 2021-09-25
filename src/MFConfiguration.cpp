51c3cff51a19031e03166f4bf53e51303807fbb7
danecreekphotography <info@danecreekphotography.com>
2021-09-25 13:42:54 +0000

    Initial test of flash write/read

--- src/MFConfiguration.cpp
+++ src/MFConfiguration.cpp
@@ -1,11 +1,18 @@
+#include <FlashIAP.h>
 #include <map>
 #include <mbed.h>
 
 #include "Globals.hpp"
 #include "MFConfiguration.hpp"
 
+#define FLASH_USER_DATA_START 0x080E0000
+#define FLASH_USER_DATA_SIZE 0x20000 // 128k
+
 #define MAX_BUFFER_SIZE 40
 char buffer[40];
+
+static uint8_t userConfig[64] __attribute__((__section__(".user_data")));
+static const char *testBuffer = "Hello";
 
 void MFConfiguration::AddButton(ARDUINO_PIN arduinoPinName, char const *name)
 {
@@ -41,6 +48,14 @@ void MFConfiguration::Load()
 
 void MFConfiguration::Erase()
 {
+  auto flash = new FlashIAP();
+
+  flash->init();
+  printf("%lu", flash->get_flash_size());
+  flash->erase(FLASH_USER_DATA_START, FLASH_USER_DATA_SIZE);
+  flash->program(testBuffer, FLASH_USER_DATA_START, sizeof(testBuffer));
+  printf("%u", (unsigned int)userConfig);
+  flash->deinit();
 }
 
 void MFConfiguration::Serialize()
