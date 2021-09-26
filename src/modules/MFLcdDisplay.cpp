babe0598f1c39f0cc3f942f3678ad8b4cca664f0
Neil Enns <info@danecreekphotography.com>
2021-09-26 08:34:10 -0700

    At least the display lights up now

--- src/modules/MFLcdDisplay.cpp
+++ src/modules/MFLcdDisplay.cpp
@@ -10,10 +10,11 @@
 
 MFLcdDisplay::MFLcdDisplay(char deviceAddress, TextLCD_Base::LCDType deviceType, std::string name)
 {
-  _deviceAddress = deviceAddress;
   _deviceType = deviceType;
   _name = name;
-
+  // mbed os uses 8-bit addressing, but MobiFlight uses 7-bit.
+  // Shift left once to convert the Arduino address to mbed os address.
+  _deviceAddress = deviceAddress << 1;
   _i2c_lcd = new I2C(I2C_SDA, I2C_SCL);
   _display = new TextLCD_I2C(_i2c_lcd, _deviceAddress, _deviceType);
 }
@@ -39,6 +40,7 @@ void MFLcdDisplay::Serialize(char *str, size_t len)
  */
 void MFLcdDisplay::StartTest()
 {
+  _display->setBacklight(TextLCD_Base::LCDBacklight::LightOn);
   _display->setCursor(TextLCD::CurOff_BlkOn);
   printf("LCD Test. Columns=%d, Rows=%d\n\r", _display->columns(), _display->rows());
 
@@ -47,6 +49,7 @@ void MFLcdDisplay::StartTest()
     int col = 0;
 
     printf("MemAddr(Col=%d, Row=%d)=0x%02X\n\r", col, row, _display->getAddress(col, row));
+    //      _display->putc('-');
     _display->putc('0' + row);
 
     for (col = 1; col < _display->columns() - 1; col++)
