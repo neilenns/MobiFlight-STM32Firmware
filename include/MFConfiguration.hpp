afc84472f6f7649dd38bcbffd16a03247247a7b5
danecreekphotography <info@danecreekphotography.com>
2021-09-29 03:12:31 +0000

    Clean up MFConfiguration

--- include/MFConfiguration.hpp
+++ include/MFConfiguration.hpp
@@ -28,22 +28,22 @@ public:
  * @brief List of connected buttons.
  * 
  */
-  map<ARDUINO_PIN, MFButton *> buttons;
+  map<ARDUINO_PIN, std::shared_ptr<MFButton>> buttons;
   /**
    * @brief List of connected LCD displays.
    * 
    */
-  map<int, MFLcdDisplay *> lcdDisplays;
+  map<int, std::shared_ptr<MFLcdDisplay>> lcdDisplays;
   /**
    * @brief List of connected MFMAX7219 displays.
    * 
    */
-  map<ARDUINO_PIN, MFMAX7219 *> ledDisplays;
+  map<ARDUINO_PIN, std::shared_ptr<MFMAX7219>> ledDisplays;
   /**
    * @brief List of connected LEDs.
    * 
    */
-  map<ARDUINO_PIN, MFOutput *> outputs;
+  map<ARDUINO_PIN, std::shared_ptr<MFOutput>> outputs;
 
   /**
  * @brief Adds a new button to the configuration.
